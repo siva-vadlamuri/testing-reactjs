76dca039aac7a91fb692d876686b4002e4149529
sivavadlamuri <sivavadlamuru1997@gmail.com>
2021-09-25 08:35:27 +0530

    added-some-text

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component{
     return(
 
       <>
+      <h2>Counter Application</h2>
       <p>count : {this.state.count}</p>
       <button className="increment" onClick={()=>{this.setState({count:this.state.count+1})}}>Increment</button>
       </>
