237eb6fe53a25d983b03f10d37c2d162a51cf7ce
sivavadlamuri <sivavadlamuru1997@gmail.com>
2021-09-25 08:25:44 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+import React from 'react';
+
+class App extends React.Component{
+  state = {
+    count : 0,
+  }
+  render(){
+    return(
+
+      <>
+      <p>count : {this.state.count}</p>
+      <button className="increment" onClick={()=>{this.setState({count:this.state.count+1})}}>Increment</button>
+      </>
+    )
+  }
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
+import App from './App';
+Enzyme.configure({ adapter: new Adapter() });
+describe('Addtion of 2 + 2 ',()=>{
+  it('I want to 2+2 is 4',()=>{
+    expect(2+2).toBe(4);
+  })
+  it('4 +2 should be 6',()=>{
+    expect(4+2).toBe(6);
+  })
+})
+
+describe('Testing App component',()=>{
+  test('Is count is 0 initialy',()=>{
+    const wrapper = shallow(<App/>);
+    const countText = wrapper.find('p').text();
+    expect(countText).toBe('count : 0')
+
+  })
+  test('Increment the count by 1',()=>{
+    const wrapper = shallow(<App/>);
+     const incrementButton = wrapper.find('button.increment');
+     incrementButton.simulate('click');
+     const countText = wrapper.find('p').text();
+     expect(countText).toBe('count : 1');
+
+  })
+})
